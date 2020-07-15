3ffde5961a2927388c1ef80404a9758a3ea4ca6b
Shane <shanedubish@gmail.com>
2020-07-15 16:35:35 -0400

    fixes

--- toolbox_dj/urls.py
+++ toolbox_dj/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django,view.generic import TemplateView
+
 
 urlpatterns = [
     path('api-auth/',include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('mytools.api.urls')),
+    re_path(".*", TemplateView.as_veiw(template_name= 'index.html'))
 ]
